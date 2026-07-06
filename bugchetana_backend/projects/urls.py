5a045e452c8ee9e98030572790912a5fd632b853
Anuja khatri <khatrianuja20@gmail.com>
2026-07-06 12:03:11 +0545

    fix: correct projects/ URL prefix so RM can create projects (404->201)

--- bugchetana_backend/projects/urls.py
+++ bugchetana_backend/projects/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', ProjectListCreateView.as_view(), name='projects-list-create'),
-    path('<int:pk>/', ProjectDetailView.as_view(), name='projects-detail'),
-    path('<int:project_id>/members/', ProjectMemberListView.as_view(), name='project-members-list'),
-    path('<int:project_id>/members/add/', AddProjectMemberView.as_view(), name='add-member'),
-    path('<int:project_id>/members/<int:user_id>/', RemoveProjectMemberView.as_view(), name='remove-member'),
+    path('projects/', ProjectListCreateView.as_view(), name='projects-list-create'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='projects-detail'),
+    path('projects/<int:project_id>/members/', ProjectMemberListView.as_view(), name='project-members-list'),
+    path('projects/<int:project_id>/members/add/', AddProjectMemberView.as_view(), name='add-member'),
+    path('projects/<int:project_id>/members/<int:user_id>/', RemoveProjectMemberView.as_view(), name='remove-member'),
 ]
